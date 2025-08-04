731891171bbc38f84b7bab29f944320230557e80
rutika-bq <ryelluma@gitam.in>
2025-08-04 14:48:49 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,12 @@
+public class ifelse
+{
+public static void main(String[] args)
+{
+int number;
+
+if (number > 0) {
+    System.out.println("Positive");
+} else {
+    System.out.println("Negative or Zero");
+}
+}
